4080a45fe5fb8aed8d281bfc5b1c1f9579a7d832
Tanmay <tanmays9999@gmail.com>
2024-09-11 22:18:52 +0530

    updating js

--- myscript.js
+++ myscript.js
@@ -7,6 +7,11 @@ var player2Color = 'rgb(237, 45, 73)';
 var game_on = true;
 var table = $('table tr');
 
+const disableBtn = () => {
+  for(let box of boxes) {
+      box.disabled = true;
+  }
+}
 
 function reportWin(rowNum,colNum) {
   console.log("You won starting at this row,col");
@@ -90,6 +95,7 @@ function gameEnd(winningPlayer) {
       $('h3').fadeOut('fast');
       $('h2').fadeOut('fast');
       $('h1').text(winningPlayer+" has won! Refresh your browser to play again!").css("fontSize", "50px")
+      disableBtn();
     }
   }
 }
